38d6d217760544292b9b8268ed45e89184fb0939
rafuchosnow <rafuchosnow@gmail.com>
2021-07-24 00:12:52 -0400

    Initial design, UML and GUI

---
+++ src/main/java/ucf/assignments/Inventory.java
@@ -0,0 +1,41 @@
+/*
+ *
+ *  *  UCF COP3330 Summer 2021 Assignment 5 Solution
+ *  *  Copyright 2021 Rafael Nieves
+ *
+ */
+
+package ucf.assignments;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Inventory extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Inventory.fxml"));
+
+            Scene scene = new Scene(root);
+
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Inventory List");
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/ucf/assignments/InventoryController.java
@@ -0,0 +1,12 @@
+/*
+ *
+ *  *  UCF COP3330 Summer 2021 Assignment 5 Solution
+ *  *  Copyright 2021 Rafael Nieves
+ *
+ */
+
+package ucf.assignments;
+
+public class InventoryController {
+
+}
